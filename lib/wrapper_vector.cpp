43a312a80171b927217a2afc877bbb20d7a6e9fa
50t3r105 <giuseppe.sodero.1997@gmail.com>
2022-03-30 17:59:01 +0200

    Bubble sort implementato:
    Issue: #CB45637488

--- lib/wrapper_vector.cpp
+++ lib/wrapper_vector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "wrapper_vector.h"
+#include <algorithm>
 
 void WrapperVector::pushBack(std::int32_t v){
     this->vec.push_back(v);
@@ -16,6 +17,15 @@ std::size_t WrapperVector::size()const{
 }
 
 void WrapperVector::sort() {
+    for (std::size_t i = 0; i< this->size(); i++){
+        for(std::size_t j = i; j< this->size(); j++){
+            if(this->at(i)>this->at(j)){
+                auto tmp = this->vec[i];
+                this->vec[i]=this->vec[j];
+                this->vec[j]=tmp;
+            } 
+        }
+    }
 }
 
 std::int32_t WrapperVector::at(std::size_t i) {
